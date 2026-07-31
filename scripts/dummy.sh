d6a5765e02b7fd8b6a6d6c409e91aa1caa018b65
root <root@ip-172-31-34-24.ec2.internal>
2026-07-31 14:37:35 +0000

    Initial commit: systemd service project setup

---
+++ scripts/dummy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LOG_FILE="/var/log/dummy-service.log"
+
+echo "Dummy service started at $(date)" >> "$LOG_FILE"
+
+while true; do
+    echo "$(date): Dummy service is running..." >> "$LOG_FILE"
+    sleep 10
+done
